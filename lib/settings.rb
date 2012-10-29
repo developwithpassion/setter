41feef79e836b7c494e551f3af35b381de53b2a0
Tristan Slominski <tristan.slominski@gmail.com>
2012-10-29 14:50:46 -0500

    initial commit of settings gem

---
+++ lib/settings.rb
@@ -0,0 +1,32 @@
+module Settings
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def set(source, destination)
+      destination.class.instance_eval do
+        @list_of_settings.each do |setting|
+          if source.respond_to? setting
+            destination.send :"#{setting.to_s}=", (source.send setting)
+          end
+        end unless not @list_of_settings
+      end
+    end
+
+    def setting(setting_name)
+      send :define_method, :"#{setting_name.to_s}=" do |value|
+        instance_variable_set "@#{setting_name.to_s}", value
+      end
+
+      send :define_method, setting_name do
+        instance_variable_get "@#{setting_name.to_s}"
+      end
+
+      self.instance_eval do
+        @list_of_settings ||= []
+        @list_of_settings << setting_name
+      end
+    end
+  end
+end
---
+++ settings.gemspec
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+lib = File.expand_path('../lib/', __FILE__)
+$:.unshift lib unless $:.include?(lib)
+
+Gem::Specification.new do |s|
+  s.name = "settings"
+  s.summary = "Module for settings magic"
+  s.version = "0.0.0"
+  s.require_paths = ["lib"]
+  s.files = Dir.glob("{lib}/**/*")
+  s.platform = Gem::Platform::RUBY
+  s.required_ruby_version = ">= 1.9.2"
+end
